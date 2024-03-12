e086e30c4ec2f19765ed0251a20ea00d5f388aaa
j30n9hn <lljhh6834@naver.com>
2024-03-12 14:46:32 +0900

    first uplaod

---
+++ src/BookMarket.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class BookMarket {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Shopping Mall");
+		System.out.println("Welcome to Book Market!");
+	}
+}
