eadb81cd66e068b1e3d73f3615e924075cf6dc40
j30n9hn <lljhh6834@naver.com>
2024-04-09 16:59:14 +0900

    관리자

--- src/Welcome.java
+++ src/Welcome.java
@@ -5,6 +5,7 @@ public class Welcome {
 	static final int NUM_ITEM = 7;
 	static CartItem[] mCartItem = new CartItem[NUM_BOOK];
 	static int mCartCount = 0;
+	static User mUser;
 
 	public static void main(String[] args) {
 		String[][] mBook = new String[NUM_BOOK][NUM_ITEM];
@@ -18,6 +19,7 @@ public class Welcome {
 
 		System.out.print("연락처를 입력하세요 : ");
 		int phone = input.nextInt();
+		mUser = new User(name, phone);
 		
 		boolean quit = false;
 
@@ -39,8 +41,8 @@ public class Welcome {
 			System.out.println("메뉴 번호를 선택하세요 : ");
 			int choice = input.nextInt();
 
-			if (choice < 1 || choice > 8) {
-				System.out.println("1부터 8까지의 숫자를 입력하세요i");
+			if (choice < 1 || choice > 9) {
+				System.out.println("1부터 9까지의 숫자를 입력하세요i");
 			} else {
 				switch (choice) {
 					case 1:
@@ -67,8 +69,12 @@ public class Welcome {
 						menuCartRemoveItem();
 						break;
 					case 8:
+						input.close();
 						menuCartExit();
 						quit = true;
+						break;
+					case 9:
+						menuAdminLogin();
 						break;
 				} // switch 끝
 			} // else 끝
@@ -86,6 +92,7 @@ public class Welcome {
 			System.out.println("2. 장바구니 상품 목록 보기\t6. 장바구니의 항목 수량 줄이기");
 			System.out.println("3. 장바구니 비우기\t7. 장바구니의 항목 삭제하기");
 			System.out.println("4. 영수증 표시하기\t8. 종료");
+			System.out.println("9. 관리자 로그인");
 			System.out.println("************************");
 	}
 
@@ -99,8 +106,9 @@ public class Welcome {
 	public static void menuGuestInfo(String name, int phone) {
 			System.out.println("1. 현재 고객정보 : ");
 			//System.out.println("이름 : " + name + " 연락처 : " + phone);
-			Person person = new Person(name, phone);
-			System.out.println("이름 " + person.getName() + " 연락처 " + person.getPhone());
+			//Person person = new Person(name, phone);
+			//System.out.println("이름 " + person.getName() + " 연락처 " + person.getPhone());
+			System.out.println("이름 " + mUser.getName() + " 연락처 " + mUser.getPhone());
 	}
 	
 	/**
@@ -164,6 +172,7 @@ public class Welcome {
 						}
 					}
 					quit = true;
+					input.close();
 				} else {
 					System.out.println("다시 입력해 주세요");
 				}
@@ -218,7 +227,23 @@ public class Welcome {
 		}
 		return flag;
 	}
-			
 
+	public static void menuAdminLogin() {
+		System.out.println("관리자 정보를 입력하세요");
 
+		Scanner input = new Scanner(System.in);
+		System.out.print("아이디 : ");
+		String adminId = input.next();
+
+		System.out.print("비밀번호 : ");
+		String adminPW = input.next();
+
+		Admin admin = new Admin(mUser.getName(), mUser.getPhone());
+		if (adminId.equals(admin.getId()) && adminPW.equals(admin.getPassword())) {
+			System.out.println("이름 " + admin.getName() + " 연락처 " + admin.getPhone());
+			System.out.println("아이디 " + admin.getId() + " 비밀번호 " + admin.getPassword());
+		} else {
+			System.out.println("관리자 정보가 일치하지 않습니다.");
+		}	
+	}
 } //wellcome 클래스 끝
